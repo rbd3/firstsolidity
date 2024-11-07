99d90c908eb1f2a733b9c8a8b9720911d3979494
rbd3 <rabedesanaandry01@gmail.com>
2024-11-07 18:35:36 +0300

    Write first function

---
+++ contracts/LearnFunction.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.27;
+
+contract LearnFunction {
+    // Contract code will go here
+    uint256 public hey;
+
+    function getInfo() public view returns (uint256) {
+        return hey;
+    }
+}
